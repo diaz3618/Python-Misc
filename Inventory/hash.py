09f4c03a028d2dea3613cfa86e1ee42eb0613d15
Daniel Diaz Santiago <daniel.diaz.stg@gmail.com>
2018-06-21 13:24:00 -0400

    N/A

--- Inventory/hash.py
+++ Inventory/hash.py
@@ -3,10 +3,10 @@ import pickle
 def save(data = [], filename = ""):
         with open(filename, "wb") as fp:
                 pickle.dump(data, fp)
-##        with open(filename, "rb") as fp:
-##                _data = pickle.load(fp)
-##      print(_data)      
-
+        with open(filename, "rb") as fp:
+                _data = pickle.load(fp)
+        print(_data)
+      
 def load(filename = ""):
         try:
                 with open(filename, "rb") as fp:
@@ -15,40 +15,63 @@ def load(filename = ""):
         except:
                 return None
 #def clear(filename = ""):
+
+
+def _hash():
+        hashVar = load("hash.inv")
+        sku = []
+        qty = []
+        last = int(len(sku) + 1)
+        
+        aSku = str(input("Sku: "))
+        sku.insert(last, aSku)
+
+        aQty = str(input("Qty: "))
+        qty.insert(last, aQty)
+                
+        addHash = {k:v for k, v in zip(sku, qty)}
+        hashVar.insert(last, addHash)
+        save(hashVar, "hash.inv")
         
 def main():
-        sku = load("sku.inv")
-        qty = [56, 52, 34, 56, 56]
-
-        """
-        Insert to lists.
-        """
-        aSku = str(input("Sku: "))
-        if aSku in sku == False:
-                _insert(sku, aSku)
-        
-        aQty = str(input("Qty: "))
-        if aQty in qty == False:
-                _insert(qty, aQty)
-
-
-        """
-        Save to files, and print to verify.
-        """
-        save(sku, "sku.inv")
-        save(qty, "qty.inv")
-        print("Sku: " + str(sku))
-        print("Qty: " + str(qty))
-
-
-        """
-        Hash
-        """
-        hash = {k:v for k, v in zip(sku, qty)}
-        print("Hash: " + str(hash['249-666']))
+        _hash()
+##        sku = load("sku.inv")
+##        last = int(len(sku) + 1)
+##        print(last)
+##        qty = [56, 52, 34, 12, 56, 12]
+##
+##        """
+##        Insert to lists.
+##        """
+##        aSku = str(input("Sku: "))
+##        if aSku not in qty:
+##                sku.insert(last, aSku)
+##        else:
+##                aSku = 0
+##
+##        aQty = str(input("Qty: "))
+##        if aQty in qty == True:
+##                _insert(qty, aQty)
+##
+##
+##        """
+##        Save to files, and print to verify.
+##        """
+##        save(sku, "sku.inv")
+##        save(qty, "qty.inv")
+##        print("Sku: " + str(sku))
+##        print("Qty: " + str(qty))
+##
+##
+##        """
+##        Hash
+##        """
+##        hash = {k:v for k, v in zip(sku, qty)}
+##        print("Hash: " + str(hash['111-222']))
 
 def _insert(var, val):
-        var.append(val)
+        last = int(len(var+1))
+        var.insert(last, str(val))
 
 if __name__ == "__main__":
         main()
