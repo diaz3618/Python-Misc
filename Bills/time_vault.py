347c94817a3b2f610861a88b6a9011adf1cd7239
Daniel Diaz Santiago <daniel.diaz.stg@gmail.com>
2018-08-12 21:36:28 -0400

    misc additions

--- Bills/time_vault.py
+++ Bills/time_vault.py
@@ -1,7 +1,43 @@
-from datetime import *
+from datetime import datetime
+from crontab import CronTab
 import time
 RATE = 11.60
 PPM = RATE / 60
+
+def main():
+    time_controller(0)
+    line = "{:5} {:>5} {:<0}"
+    work_hrs = float(input("Work hours: "))
+    mins = float(time_vault(work_hrs * 60))
+    print(mins)
+
+    print(line.format("\n\nWorked:", str(work_hrs), "hrs"))
+    print(line.format("Wallet:", "$", str(round(PPM * mins, 3))))
+
+
+def time_controller(mins):
+    cron = CronTab(user=True)
+    job  = cron.new(time_vault(mins))
+    job.minute.during(5,50).every(5)
+    job.hour.every(4)
+    job.day.on(4, 5, 6)
+
+    job.dow.on('SUN')
+    job.dow.on('SUN', 'FRI')
+    job.month.during('APR', 'NOV')
+
+    
+    now = datetime.now().strftime('%I:%M:%S %p')
+    shift_start = datetime.strptime("09:15:00 PM", "%I:%M:%S %p")
+    print(now)
+    print(shift_start)
+    while str(now) != '09:14:00 PM':
+        #print("Waiting...")
+
+        if str(now) == '09:14:00 PM':
+            print("Time is now: " + str(now))
+            break
+
 
 def time_vault(_max):
     secs = 0
@@ -36,14 +72,6 @@ def time_vault(_max):
         if time_vault == _max*60:
             return time_vault
 
-def main():
-    line = "{:5} {:>5} {:<0}"
-    work_hrs = float(input("Work hours: "))
-    mins = float(time_vault(work_hrs * 60))
-    print(mins)
-
-    print(line.format("\n\nWorked:", str(work_hrs), "hrs"))
-    print(line.format("Wallet:", "$", str(round(PPM * mins, 3))))
 
 if __name__ == "__main__":
     main()
